7029c15c51f40b34552fcc0cf9695c91cfde06bb
Lee <lhwilli4@cougarnet.uh.edu>
2025-07-13 22:10:00 -0500

    test

--- dataset.py
+++ dataset.py
@@ -27,3 +27,75 @@ class TrainDataset(Dataset): #will chain torch transforms in main
         if self.training:
             sample = self.augment(sample)
         return sample
+
+    def show_sample(self, plane, slice_idx=None):
+        stack_img = sample["image"]
+        seg_img = sample["seg"]
+    
+        if plane == 'axial':
+            max_slice = stack_img.shape[1] - 1
+            if slice_idx is None:
+                # slice_idx = stack_img.shape[1] // 2
+                slice_idx = max_slice // 2
+        elif plane == 'coronal':
+            max_slice = stack_img.shape[2] - 1
+            if slice_idx is None:
+                #slice_idx = stack_img.shape[2] // 2
+                slice_idx = max_slice//2
+        elif plane == 'sagittal':
+            max_slice = stack_img.shape[3] - 1
+            if slice_idx is None:
+                #slice_idx = stack_img.shape[3] // 2
+                slice_idx = max_slice//2
+        else:
+            raise ValueError(f"Invalid plane: {plane}")
+
+        def get_slice(volume, slice_idx, channelIndex):
+            if plane == 'axial':
+                return volume[channelIndex, slice_idx, :, :]  # shape (D, H, W)
+            elif plane == 'coronal':
+                img_coronal = volume[channelIndex, :, slice_idx, :]
+                #img_coronal = img_coronal.permute(1,0)
+                return img_coronal
+                #return volume[:, :, slice_idx, :]
+            elif plane == 'sagittal':
+                img_sagittal = volume[channelIndex, :, :, slice_idx]
+                #img_sagittal = img_sagittal.permute(1,0)
+                return img_sagittal
+                #return volume[:, :, :, slice_idx]
+            else:
+                raise ValueError(f"Invalid plane: {plane}")
+
+        def get_seg_slice(seg, slice_idx):
+            if plane == 'axial':
+                return seg[slice_idx, :, :]      # [D, H, W] → axial = D
+            elif plane == 'coronal':
+                seg_coronal = seg[:, slice_idx, :]
+                #seg_coronal = seg_coronal.permute(1,0)
+                return seg_coronal
+                #return seg[:, slice_idx, :]      # H
+            elif plane == 'sagittal':
+                seg_sagittal = seg[:, :, slice_idx]
+                #seg_sagittal = seg_sagittal.permute(1,0)
+                return seg_sagittal
+                #return seg[:, :, slice_idx]      # W
+            else:
+                raise ValueError(f"Invalid plane: {plane}")
+
+        img_slices = [None] * 4
+        for index in range(4):
+            img_slices[index] = get_slice(stack_img, slice_idx, index)
+        seg_slice = get_seg_slice(seg_img, slice_idx)
+        titles = ["FLAIR", "T1", "T1CE", "T2"]
+        fig, axes = plt.subplots(1, 5, figsize=(18, 4))
+
+        for i in range(4):
+            axes[i].imshow(img_slices[i].cpu(), cmap="gray")
+            axes[i].set_title(f"{titles[i]} ({plane}, {slice_idx})")
+
+        axes[4].imshow(seg_slice.cpu(), cmap="gray")
+        axes[4].set_title("Segmentation")
+        axes[4].axis("off")
+
+        plt.tight_layout()
+        plt.show()
